eb451d3cb4423de17b46709f0ac50a20b8a48efc
Yassir Rouane <yasserro100@gmail.com>
2025-08-04 21:13:33 +0100

    feat: add hero section with background image

--- apps/web/src/app/(public)/page.tsx
+++ apps/web/src/app/(public)/page.tsx
@@ -1,6 +1,8 @@
-'use client';
+"use client";
 
-import { MainHeader } from '@/widgets';
+import Image from "next/image";
+import "./style.css";
+import { MainHeader } from "@/widgets";
 
 export default function Home() {
   return (
@@ -8,7 +10,16 @@ export default function Home() {
       <MainHeader />
 
       <main>
-        <section> </section>
+        <section id="hero-section">
+          <Image
+            alt="Hero Image"
+            className="hero-image"
+            fill
+            src="/svg/hero-bg.svg"
+          />
+        </section>
+
+        <section id="categories-section"> </section>
       </main>
 
       <footer> </footer>
